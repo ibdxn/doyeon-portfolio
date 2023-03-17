f38bfb65d722bfd149a98e7d1e2a08758e6a667b
Doyeon <ibdxn@naver.com>
2023-03-17 22:36:32 +0900

    fix

--- doyeon-portfolio/next.config.js
+++ doyeon-portfolio/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== "production";
-const repository = "doyeon-portfolio";
+const repository = "resume";
 
 
 const nextConfig = {
